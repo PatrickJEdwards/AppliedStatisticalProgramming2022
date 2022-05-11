1e1b0267ca11b6b533403edef197e83fbd54a510
Patrick Edwards <edwards.p@wustl.edu>
2022-05-10 19:32:24 -0500

    Cleaning up dev file.

--- PS_5625_PS6/rcppEBMA_dev.R
+++ PS_5625_PS6/rcppEBMA_dev.R
@@ -9,20 +9,20 @@ setwd("~/GitHub/AppliedStatisticalProgramming2022/PS_5625_PS6")
 getwd()
 
 ## Create Package:
-funcDir <- "~/1. Washington University in St. Louis/5. Spring 2022 Semester/Pol Sci 5625 Applied Statistical Programming/rcppEBMA/"
-Rcpp.package.skeleton(
-  name = "rcppEBMA",
-  cpp_files = c(
-    paste(funcDir, "Z_H.h", sep = ""),
-    paste(funcDir, "CPP_EBMA.cpp", sep = ""),
-    paste(funcDir, "W_H.cpp", sep = ""),
-    paste(funcDir, "Z_H.h", sep = ""),
-    paste(funcDir, "Z_H.cpp", sep = ""),
-    paste(funcDir, "Z_H.h", sep = "")
-  ),
-  author = "Patrick Edwards",
-  email = "edwards.p@wustl.edu"
-)
+#funcDir <- "~/1. Washington University in St. Louis/5. Spring 2022 Semester/Pol Sci 5625 Applied Statistical Programming/rcppEBMA/"
+#Rcpp.package.skeleton(
+#  name = "rcppEBMA",
+#  cpp_files = c(
+#    paste(funcDir, "Z_H.h", sep = ""),
+#    paste(funcDir, "CPP_EBMA.cpp", sep = ""),
+#    paste(funcDir, "W_H.cpp", sep = ""),
+#    paste(funcDir, "Z_H.h", sep = ""),
+#    paste(funcDir, "Z_H.cpp", sep = ""),
+#    paste(funcDir, "Z_H.h", sep = "")
+#  ),
+#  author = "Patrick Edwards",
+#  email = "edwards.p@wustl.edu"
+#)
 
 
 ## This code can be run as many times as desired.
@@ -44,5 +44,5 @@ document(current.code) # Run to fill/update `MAN` files.
 
 
 # Load functions from package:
-load_all()
+load_all("rcppEBMA")
 
