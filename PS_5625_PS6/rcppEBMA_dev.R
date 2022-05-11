dafbfe1e669eb8fedf94ed1fccc98bec93a7e99f
Patrick Edwards <edwards.p@wustl.edu>
2022-05-10 20:04:38 -0500

    Trying to create a good dev file that demonstrated function use

--- PS_5625_PS6/rcppEBMA_dev.R
+++ PS_5625_PS6/rcppEBMA_dev.R
@@ -4,9 +4,61 @@ library(devtools)
 #install.packages("roxygen2")
 library(roxygen2)
 
+
 # SET TO YOUR OWN WORKING DIRECTORY
 setwd("~/GitHub/AppliedStatisticalProgramming2022/PS_5625_PS6")
 getwd()
+
+
+## CP_EBMA Dev File: Demonstration of Functionality:
+
+# Load test data: Presidential Forecasts data from Montgomery et al. (2012).
+library(EBMAforecast)
+df <- EBMAforecast::presidentialForecast
+
+# Preliminaries:
+load_all("rcppEBMA")
+
+y <- as.matrix(df$Actual) # Actual Outcomes.
+x <- as.matrix(df[,1:6]) # Predictions
+K <- dim(df)[2] - 1 # Num. of Models (columns of x)
+N <- dim(df)[1] # Num. of observations (rows)
+
+# Initial weights (assumed equal predictive power):
+w_hat <- replicate(K,1/K)
+
+# Test Z-hat function:
+zhatTest <- Z_H(
+  x = x,
+  y = y,
+  W_H = w_hat, 
+  sd = 1
+)
+
+# Test W-at (weighting) function:
+whatTest <- W_H(Z_H = zhatTest)
+
+# Test whole EBMA function:
+CPP_EBMA_Test <- CPP_EBMA(
+  x = x,
+  y = y,
+  W_Hats = w_hat,
+  sd = 1,
+  tol = 0.0
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ## Create Package:
 #funcDir <- "~/1. Washington University in St. Louis/5. Spring 2022 Semester/Pol Sci 5625 Applied Statistical Programming/rcppEBMA/"
@@ -45,4 +97,14 @@ document(current.code) # Run to fill/update `MAN` files.
 
 # Load functions from package:
 load_all("rcppEBMA")
+
+# Check documentation files:
+?rcppEBMA
+?rcppEBMA::Z_H
+?rcppEBMA::W_H
+?rcppEBMA::CPP_EBMA
+
+
+
+
 
